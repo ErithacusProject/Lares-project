65ccd4f29cab913e9067704f9f37fca5e3db8862
Erithacus <92083124+ErithacusProject@users.noreply.github.com>
2024-10-10 19:31:32 +0100

    Update battle info.cs

--- Lares/Assets/battle info.cs
+++ Lares/Assets/battle info.cs
@@ -5,16 +5,21 @@ namespace Lares
 
 public struct AttackInfo
 {
-public int AttackValue {get; private set;) //the attackers physical or seri attack value 
+public int attackValue { get; private set; } // the attackers physical attack or magic attack stat
+public int attackPower {get; private set;) //the power of the attack
+public int weaponModifier { get; private set; } // physical or seri modifier of the equipped weapon 
 public bool crit { get; private set; }
-
+public int attackCritMod { get; private set; } 
+public int weaponCritMod { get; private set; } 
 }
 
 public class BattleInfo : MonoBehaviour
 {
 public int level { get; private set; }
 public int HP { get; private set; } 
+public int maxHP { get; private set; }
 public int SP { get; private set; } 
+public int maxSP { get; private set; } 
 public int EXP { get; private set; } 
 public int physicalAttack { get; private set; }
 public int physicalDefense { get; private set; } 
@@ -23,14 +28,15 @@ public int SeriDefense { get; private set; }
 public int agility { get; private set; }
 public int luck { get; private set; } 
 
-public SpecialAttack(AttackInfo attack) 
+public SeriAttack(AttackInfo attack) 
 {
-// performes on you to cause HP loss 
+// performed on you to cause HP loss 
 } 
 
-public PhysicalAttack(AttackInfo) 
+public PhysicalAttack(AttackInfo attack) 
 {
-}
 
 }
+}
+
 }
