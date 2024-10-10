c47ed0f7e31d2ce09ad55868a584bcb4d6a1096b
Erithacus <92083124+ErithacusProject@users.noreply.github.com>
2024-10-10 08:14:38 +0100

    battle script wip

---
+++ Lares/Assets/battle info.cs
@@ -0,0 +1,19 @@
+
+
+namespace Lares
+{
+public class BattleInfo : MonoBehaviour
+{
+public int level { get; private set; }
+public int HP { get; private set; } 
+public int SP { get; private set; } 
+public int EXP { get; private set; } 
+public int physicalAttack { get; private set; }
+public int physicalDefense { get; private set; } 
+public int SeriAttack { get; private set; } 
+public int SeriDefense { get; private set; }
+public int agility { get; private set; }
+public int luck { get; private set; } 
+
+}
+}
